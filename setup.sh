7f483b4bfedd86d57e2580042a4df25e9ae32850
Michael Shang <mshang@gmail.com>
2019-07-22 15:27:39 -0700

    Add more git settings

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+# git 
 git config --global alias.co checkout
 git config --global alias.br branch
 git config --global alias.st status
@@ -10,3 +11,8 @@ git config --global user.name "XXX YYY"
 
 git config --local user.email "XXX@YYY"
 git config --local user.name "XXX YYY"
+git config --global core.editor "vim"
+
+# bash git prompt
+# https://github.com/magicmonty/bash-git-prompt
+git clone https://github.com/magicmonty/bash-git-prompt.git ~/.bash-git-prompt --depth=1
